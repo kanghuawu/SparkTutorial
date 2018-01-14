cac723c3c676e9fd995ffab61d13d11fadd90d02
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-13 17:30:58 -0800

    complete sum of numbers

--- src/main/java/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.java
+++ src/main/java/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.java
@@ -1,5 +1,10 @@
 package com.sparkTutorial.rdd.sumOfNumbers;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+
 public class SumOfNumbersProblem {
 
     public static void main(String[] args) throws Exception {
@@ -9,5 +14,13 @@ public class SumOfNumbersProblem {
 
            Each row of the input file contains 10 prime numbers separated by spaces.
          */
+        SparkConf conf = new SparkConf().setAppName("airportusa").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        int sum = sc.textFile("in/prime_nums.text")
+                .flatMap(line -> Arrays.asList(line.split("\\s+")).iterator())
+                .filter(num -> !num.isEmpty())
+                .map(num -> Integer.valueOf(num))
+                .reduce((x, y) -> x + y);
+        System.out.println(sum);
     }
 }
